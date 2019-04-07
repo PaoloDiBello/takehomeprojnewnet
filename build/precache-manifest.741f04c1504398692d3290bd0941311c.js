640baec596baa94d7c60ecc25aafc4fb84e016d3
PaoloDiBello <dibellopol@gmail.com>
2019-04-07 15:26:43 +0200

    .

---
+++ build/precache-manifest.741f04c1504398692d3290bd0941311c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "434e2ef6003310781ecd",
+    "url": "/takehomeprojnewnet/static/js/runtime~main.59be5f51.js"
+  },
+  {
+    "revision": "8b49716222622222d601",
+    "url": "/takehomeprojnewnet/static/js/main.2f1f2c8d.chunk.js"
+  },
+  {
+    "revision": "0d0d402d0148c1eb2efc",
+    "url": "/takehomeprojnewnet/static/js/2.fe6c46a8.chunk.js"
+  },
+  {
+    "revision": "8b49716222622222d601",
+    "url": "/takehomeprojnewnet/static/css/main.e80ce6db.chunk.css"
+  },
+  {
+    "revision": "980a83edbad7cbee21d8063ff5f91faf",
+    "url": "/takehomeprojnewnet/index.html"
+  }
+];
--- src/ListPhotos.js
+++ src/ListPhotos.js
@@ -4,6 +4,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import LinearIndeterminate from './Loader.js'
 export default class ListPhotos extends Component {
 
     state = {
@@ -51,9 +53,11 @@ export default class ListPhotos extends Component {
         // console.log(this.props.input);
 
         return (
-            <List style={{ position: 'absolute', display: 'block', margin: 'auto' }}>
-                {this.state.filteredPhotos.map((photo, i) => <ListItem style={{ display: 'block ' }}> <ListItemText key={i} onClick={this.handleClick.bind(this, i)} style={{ cursor: 'pointer' }}>{photo.title} </ListItemText> {this.state.clicked === i && <ListItemIcon><img src={photo.url} ></img></ListItemIcon>} </ListItem>)}
-            </List>
+            <React.Fragment>
+                {this.state.photos.length > 0 ? (<List style={{ position: 'absolute', display: 'block', margin: 'auto' }}>
+                    {this.state.filteredPhotos.map((photo, i) => <ListItem style={{ display: 'block ' }}> <ListItemText key={i} onClick={this.handleClick.bind(this, i)} style={{ cursor: 'pointer' }}>{photo.title} </ListItemText> {this.state.clicked === i && <ListItemIcon><img src={photo.url} ></img></ListItemIcon>} </ListItem>)}
+                </List>) : <LinearIndeterminate />
+                }    </React.Fragment>
         )
     }
 }
